3e311a36cb5b682af45eb01027637d377048ac68
renoir01 <r.kaze@alustudent.com>
2025-07-24 21:31:28 +0200

    Change Azure location to West US 2 to avoid PostgreSQL restriction

--- infrastructure/azure/import_existing_resources.ps1
+++ infrastructure/azure/import_existing_resources.ps1
@@ -28,6 +28,33 @@ terraform import azurerm_network_security_group.db "/subscriptions/ef573996-fc64
 Write-Host "Importing Public IP..." -ForegroundColor Yellow
 terraform import azurerm_public_ip.app_gateway "/subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/publicIPAddresses/agritrace-dev-appgw-pip"
 
+# Subnets
+Write-Host "Importing App Gateway Subnet..." -ForegroundColor Yellow
+terraform import azurerm_subnet.app_gateway "/subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/virtualNetworks/agritrace-dev-vnet/subnets/agritrace-dev-appgw-subnet"
+
+Write-Host "Importing Public Subnet..." -ForegroundColor Yellow
+terraform import azurerm_subnet.public "/subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/virtualNetworks/agritrace-dev-vnet/subnets/agritrace-dev-public-subnet"
+
+Write-Host "Importing Private Subnet..." -ForegroundColor Yellow
+terraform import azurerm_subnet.private "/subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/virtualNetworks/agritrace-dev-vnet/subnets/agritrace-dev-private-subnet"
+
+Write-Host "Importing Database Subnet..." -ForegroundColor Yellow
+terraform import azurerm_subnet.database "/subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/virtualNetworks/agritrace-dev-vnet/subnets/agritrace-dev-db-subnet"
+
+# Key Vault Secrets
+Write-Host "Importing ACR Username Secret..." -ForegroundColor Yellow
+terraform import azurerm_key_vault_secret.acr_username "https://agritrace-dev-kv.vault.azure.net/secrets/acr-username/b68a3dd786364a1a99e076e9cb1b5445"
+
+Write-Host "Importing ACR Password Secret..." -ForegroundColor Yellow
+terraform import azurerm_key_vault_secret.acr_password "https://agritrace-dev-kv.vault.azure.net/secrets/acr-password/2d4ab0d94d2b4c5db127e2d2c2a6721b"
+
+Write-Host "Importing DB Password Secret..." -ForegroundColor Yellow
+terraform import azurerm_key_vault_secret.db_password "https://agritrace-dev-kv.vault.azure.net/secrets/db-password/413331a4d3494a8d90ed6b65d6752375"
+
+# Private DNS Zone Virtual Network Link
+Write-Host "Importing Private DNS Zone Virtual Network Link..." -ForegroundColor Yellow
+terraform import azurerm_private_dns_zone_virtual_network_link.postgresql "/subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/privateDnsZones/privatelink.postgres.database.azure.com/virtualNetworkLinks/agritrace-dev-psql-dns-link"
+
 Write-Host "Import completed! Verifying imported resources..." -ForegroundColor Green
 terraform state list
 
