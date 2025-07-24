a46d6aad8f2abaaa2b2870344bf4476930a2a15f
renoir01 <r.kaze@alustudent.com>
2025-07-24 21:06:47 +0200

    Change Azure location to West US 2 to avoid PostgreSQL restriction

---
+++ infrastructure/azure/import_existing_resources.ps1
@@ -0,0 +1,41 @@
+# Import existing Azure resources into Terraform state
+# Run this script from the infrastructure/azure directory
+
+Write-Host "Starting import of existing Azure resources..." -ForegroundColor Green
+
+# Resource Group
+Write-Host "Importing Resource Group..." -ForegroundColor Yellow
+terraform import azurerm_resource_group.main /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg
+
+# Container Registry
+Write-Host "Importing Container Registry..." -ForegroundColor Yellow
+terraform import azurerm_container_registry.main /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.ContainerRegistry/registries/agritracedevacr
+
+# Private DNS Zone
+Write-Host "Importing Private DNS Zone..." -ForegroundColor Yellow
+terraform import azurerm_private_dns_zone.postgresql /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/privateDnsZones/privatelink.postgres.database.azure.com
+
+# Key Vault
+Write-Host "Importing Key Vault..." -ForegroundColor Yellow
+terraform import azurerm_key_vault.main /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.KeyVault/vaults/agritrace-dev-kv
+
+# Virtual Network
+Write-Host "Importing Virtual Network..." -ForegroundColor Yellow
+terraform import azurerm_virtual_network.main /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/virtualNetworks/agritrace-dev-vnet
+
+# Network Security Group - App
+Write-Host "Importing App Network Security Group..." -ForegroundColor Yellow
+terraform import azurerm_network_security_group.app /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/networkSecurityGroups/agritrace-dev-app-nsg
+
+# Network Security Group - DB
+Write-Host "Importing DB Network Security Group..." -ForegroundColor Yellow
+terraform import azurerm_network_security_group.db /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/networkSecurityGroups/agritrace-dev-db-nsg
+
+# Public IP
+Write-Host "Importing Public IP..." -ForegroundColor Yellow
+terraform import azurerm_public_ip.app_gateway /subscriptions/ef573996-fc64-430c-88d1-a5da1fc15677/resourceGroups/agritrace-dev-rg/providers/Microsoft.Network/publicIPAddresses/agritrace-dev-appgw-pip
+
+Write-Host "Import completed! Verifying imported resources..." -ForegroundColor Green
+terraform state list
+
+Write-Host "You can now run 'terraform plan' to see what changes need to be made." -ForegroundColor Green
